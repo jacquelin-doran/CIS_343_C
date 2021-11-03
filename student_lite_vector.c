0e5bf68cb82c3392a9a8dc8555bec19d8d3369b1
jacquelin-jimenez <jacquelinpeace@gmail.com>
2021-11-03 15:28:04 -0400

    student vector updated

--- student_lite_vector.c
+++ student_lite_vector.c
@@ -4,22 +4,37 @@
 #include <string.h>
 
 lite_vector* lv_new_vec(size_t type_size){
+	lite_vector vector = malloc(type_size);
+	vector -> length = 0;
+	vector -> max_capacity = 8;
+	vector -> type_size;
+	vector -> data;
+	return vector;
+
+}
+ void lv_cleanup(lite_vector* vec){
+	free(vec);
 }
 
-void lv_cleanup(lite_vector* vec){
-}
 
 size_t lv_get_length(lite_vector* vec){
+	return vec -> lenght;
 }
 
 bool lv_clear(lite_vector* vec){
+	free(vec);
+	return true;
 }
 
 void* lv_get(lite_vector* vec, size_t index){
+	return vec -> data[index];
 }
 
 static bool lv_resize(lite_vector* vec){
+	return 1;
 }
 
 bool lv_append(lite_vector* vec, void* element){
+	return true;
 }
+
